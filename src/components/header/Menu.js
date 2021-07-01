d6dc60fa4d7c458b2a8597f7e06d1487afb2d85c
JulianadFreitas <julianachristello@gmail.com>
2021-07-01 16:12:13 -0300

    LogOut

--- src/components/header/Menu.js
+++ src/components/header/Menu.js
@@ -1,60 +1,81 @@
 import React from "react";
 import styled from "styled-components";
 import InputSearch from "./InputSearch";
+import axios from "axios";
 import { useHistory } from "react-router";
 
-import {AiOutlineShoppingCart,AiOutlineUser
-} from "react-icons/ai";
+import { AiOutlineShoppingCart, AiOutlineUser } from "react-icons/ai";
 
 export default function Menu() {
   let history = useHistory();
-  const { name } = localStorage;
-  
-  function signOut(){
-    return name
+  const { name, token } = localStorage;
+
+  function signOut() {
+    const config = {
+      headers : { Authorization : `Bearer ${token}`}
+    };
+    const request = axios.post(`${process.env.REACT_APP_API_BASE_URL}/sign-out`, 
+    {}, config);
+    request.then(()=> {
+      localStorage.clear();
+      console.log(localStorage);
+    } );
   }
+
+
   return (
-      <StyledMenuBox>
-          <InputSearch />
+    <StyledMenuBox>
+      <InputSearch />
       <Options>
-       <p>Olá, {name === undefined ? name : "visitante"} </p>
-      <button width= "80" onClick={()=> signOut}> {name === undefined ? "Sair": "Entrar"} </button>
-      <button  onClick={()=> history.push("/sign-up")}><UserIcon />Minha conta</button >
-      {name === undefined ? <button  onClick={()=> history.push("/sign-up")}><UserIcon />Meus pedidos</button > : ""}
-      <button  onClick={()=> history.push("/")}><MarketIcon />Meu carrinho</button >
+        <p>Olá, {name === undefined ? name : "visitante"} </p>
+        <button width="80" onClick={() => signOut}>
+          {name === undefined ? "Sair" : "Entrar"}
+        </button>
+        <button onClick={() => history.push("/sign-up")}>
+          <UserIcon />
+          Minha conta
+        </button>
+        {name === undefined ? (
+          <button onClick={() => history.push("/sign-up")}>
+            <UserIcon />
+            Meus pedidos
+          </button>
+        ) : (
+          ""
+        )}
+        <button onClick={() => history.push("/")}>
+          <MarketIcon />
+          Meu carrinho
+        </button>
       </Options>
-      
-  </StyledMenuBox>
-    
+    </StyledMenuBox>
   );
 }
 const StyledMenuBox = styled.form`
-width: 100%;
-height: 40px;
-margin-bottom: 30px;
-display: flex;
-justify-content: space-between;
-align-items: center;
-font-size: 13px;
-
+  width: 100%;
+  height: 40px;
+  margin-bottom: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 13px;
 `;
 const Options = styled.div`
-width: 50%;
-display: flex;
-text-align: center;
-align-items: center;
-justify-content: space-between;
+  width: 50%;
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: space-between;
 
-button{
-display: flex;
-cursor: pointer;
-display: flex;
-text-align: center;
-align-items: center;
-border:none;
-background-color: #fff;
-}
-
+  button {
+    display: flex;
+    cursor: pointer;
+    display: flex;
+    text-align: center;
+    align-items: center;
+    border: none;
+    background-color: #fff;
+  }
 `;
 const MarketIcon = styled(AiOutlineShoppingCart)`
   font-size: 13px;
@@ -66,4 +87,3 @@ const UserIcon = styled(AiOutlineUser)`
   cursor: pointer;
   margin-right: 5px;
 `;
-
